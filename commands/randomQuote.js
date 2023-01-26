d4348e35172615ac6a5afa779542ea8d687d532f
Estefan Valencia <evalenci@emich.edu>
2023-01-26 14:28:10 -0500

    feat: You can now ask the bot for a random quote submitted by any user
    * feat: You can now ask the bot for a random quote submitted by any user
    * Added new slash command called, "randomQuote.js"

---
+++ commands/randomQuote.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Quote = require("../schemas/quotes.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('randomquote')
+		.setDescription('Gives you some fuel to blackmail someone with what they said out of context....'),
+
+	async execute(interaction) {
+		const { quotes } =
+			interaction.db ??
+			(await Quote.findOneAndUpdate(
+				{ _id: interaction.guild.id },
+				{},
+				{ upsert: true, new: true },
+			));
+
+		const randNum = Math.ceil(Math.random() * quotes.length);
+		const randomQuote = quotes[ randNum - 1];
+
+
+		await interaction.reply('"' + randomQuote.text + '" -' + randomQuote.author);
+	},
+};
